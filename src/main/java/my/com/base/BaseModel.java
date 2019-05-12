3a5a5ac1993854e6ad81c0397fb7abda90b030eb
zyq002 <87518156@qq.com>
2019-05-12 15:57:00 +0800

    b

--- src/main/java/my/com/base/BaseModel.java
+++ src/main/java/my/com/base/BaseModel.java
@@ -1,50 +1,23 @@
 package my.com.base;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 
-import my.com.Util.DateUtil;
-
+import lombok.Getter;
+import lombok.Setter;
+@Getter 
+@Setter
 public abstract class BaseModel {
-
-	protected LocalDateTime createTime;
-
+	protected Long id;
+	@TableField(value = "createTime", fill = FieldFill.INSERT)
+	protected LocalDateTime  createTime;
+	protected String createCode;
+	@TableField(value = "updateTime", fill = FieldFill.UPDATE)
 	protected LocalDateTime updateTime;
-	@TableField(exist = false)
-	protected String createTimeStr;
-	@TableField(exist = false)
-	protected String updateTimeStr;
-
-	protected String getCreateTimeStr() {
-		if (this.getCreateTime() == null)
-			return null;
-		this.createTimeStr = this.getCreateTime().format(DateTimeFormatter.ofPattern(DateUtil.FORMAT_LONG));
-		return createTimeStr;
-	}
-
-	protected String getUpdateTimeStr() {
-		if (this.getUpdateTime() == null)
-			return null;
-		this.updateTimeStr = this.getUpdateTime().format(DateTimeFormatter.ofPattern(DateUtil.FORMAT_LONG));
-		return updateTimeStr;
-	}
-
-	public LocalDateTime getCreateTime() {
-		return createTime;
-	}
-
-	public void setCreateTime(LocalDateTime createTime) {
-		this.createTime = createTime;
-	}
-
-	public LocalDateTime getUpdateTime() {
-		return updateTime;
-	}
-
-	public void setUpdateTime(LocalDateTime updateTime) {
-		this.updateTime = updateTime;
-	}
-
+	protected String updateCode;
+	@TableLogic
+	protected int isDel;
 }
