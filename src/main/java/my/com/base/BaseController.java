4bb8ede1a24a0bd78efd119641c54688c016e62f
ni.jie <ni.jie@DX009-PC.dx.com>
2019-05-15 17:13:32 +0800

    新增

---
+++ src/main/java/my/com/base/BaseController.java
@@ -0,0 +1,84 @@
+package my.com.base;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.View;
+
+import org.apache.ibatis.annotations.Lang;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import my.com.common.User;
+
+public class BaseController {
+
+	public Logger logger = Logger.getLogger(BaseController.class);
+
+	@Autowired
+	HttpServletRequest request;
+
+	@Autowired
+	HttpServletResponse response;
+
+	/**
+	 * http相关
+	 * 
+	 */
+	protected String base() {
+		return request.getContextPath();
+	}
+
+	protected HttpServletRequest request() {
+		return request;
+	}
+
+	protected HttpServletResponse response() {
+		return response;
+	}
+
+	/**
+	 * 获取用户IP
+	 * 
+	 */
+	public String ip() {
+		return null;
+	}
+
+	/**
+	 * 获取用户信息
+	 * 
+	 */
+	public User user() {
+		return null;
+	}
+
+	/**
+	 * Cookie相关
+	 * 
+	 */
+	protected void addCookie(String name, String value, int age) {
+
+	}
+
+	protected String getCookie(String name) {
+		return null;
+	}
+
+	/**
+	 * Session 相关操作 getSession();putSession();removeSession();
+	 * 
+	 */
+	protected void getSession() {
+		request.getSession();
+	}
+
+	protected void removeSession(String name) {
+		request.getSession().removeAttribute(name);
+		;
+	}
+
+	protected void putSession(String key, Object value) {
+		request.getSession().setAttribute(key, value);
+	}
+}
